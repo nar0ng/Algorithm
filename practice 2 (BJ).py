d3bd79ed928f58940ebb510fa49f0c36964a6d3e
nar0ng <criticalpxint@naver.com>
2020-10-15 21:00:36 +0900

    algorithm practice

---
+++ practice 2 (BJ).py
@@ -0,0 +1,17 @@
+A = 7 #문제 1
+B = 3
+
+print (A + B)
+print (A - B)
+print (A * B)
+print (A / B)
+print (A % B)
+
+A = 5 #문제 2
+B = 8
+C = 4
+
+print ((A + B)%C)
+print (((A % C) + (B % C))%C)
+print ((A * B)%C)
+print (((A % C) * (B % C))%C)
---
+++ practice 3 (BJ).py
@@ -0,0 +1,27 @@
+#두 수 비교하기
+
+A = 1
+B = 2
+
+if A > B:
+     print ('>')
+elif A < B:\
+    print ('<')
+elif A == B:\
+    print ('=')
+
+
+#시험 성적
+
+score = 100
+
+if 90 <= score <= 100:
+    print ('A')
+elif 80 <= score <= 89:
+    print ('B')
+elif 70 <= score <= 79:
+    print('C')
+elif 60 <= score <= 69:
+    print ('D')
+elif 0 <= score <= 59:
+    print ('F')
